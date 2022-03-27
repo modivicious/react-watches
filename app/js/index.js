5fc77818e03b9021c97863aae804ecea5e4b1e5b
modivicious <prihood@bk.ru>
2022-03-27 23:23:54 +0300

    init project

---
+++ app/js/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import "../scss/style.scss"
+
+ReactDOM.render(
+  <>
+    <h1>Whatever</h1>
+  </>,
+  document.getElementById('root')
+)
